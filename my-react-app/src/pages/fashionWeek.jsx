629249d1a3224bf304aaaea16d9d424358ae34e6
Jennifer Nguyen <Tuyetnn2003@gmail.com>
2026-03-27 09:16:44 -0500

    updated the NY and London colection

--- my-react-app/src/pages/fashionWeek.jsx
+++ my-react-app/src/pages/fashionWeek.jsx
@@ -55,6 +55,43 @@ const CITY_DESIGNERS = {
                   "fall2024/couture":["women"],
         }
       },
+  { initial: "A",
+      name:"Altuzarra",
+      link:"",
+      categories:{"fall2026": ["rtw"],          "spring2026": ["rtw"], 
+                  "fall2025": ["rtw"],          "spring2025": ["rtw"], 
+                  "fall2024": ["rtw"],          "spring2024": ["rtw"] },
+
+      seasons:   ["fall2026", "spring2026", 
+                  "fall2025", "spring2025",
+                  "fall2024", "spring2024"],
+
+      subcategories:{
+                  "fall2026/rtw": ["women"],   "spring2026/rtw": ["women"],
+                  "fall2025/rtw": ["women"],   "spring2025/rtw": ["women"],
+                  "fall2024/rtw": ["women"],   "spring2024/rtw": ["women"],
+        }
+    },
+
+    {initial: "AS",
+      name: "Anna Sui",
+      link: "",
+      categories:{"fall2026": ["rtw","resort"],     "spring2026": ["rtw","resort"], 
+                  "fall2025": ["rtw","resort"],     "spring2025": ["rtw","resort"],
+                  "fall2024": ["rtw","resort"],     "spring2024": ["rtw","resort"] },
+
+      seasons:["fall2026", "spring2026", 
+              "fall2025", "spring2025", 
+              "fall2024", "spring2024"],
+      subcategories: {
+              "fall2026/rtw":   ["women"],      "spring2026/rtw":   ["women"],
+              "fall2026/resort":["women"],      "spring2026/resort":["women"],
+              "fall2025/rtw":   ["women"],      "spring2025/rtw":   ["women"], 
+              "fall2025/resort":["women"],      "spring2025/resort":["women"],
+              "fall2024/rtw":   ["women"],       "spring2024/rtw":   ["women"],
+              "fall2024/resort":["women"],      "spring2024/resort":["women"],
+        }
+      },
 
     {initial: "C",
       name:"Coach",
@@ -76,6 +113,85 @@ const CITY_DESIGNERS = {
                   "fall2024/resort":["women"],   "spring2024/resort":["women"],
         }
       },
+
+    {initial: "CH",
+      name: "Carolina Herrera",
+      link: "",
+      categories:{"fall2026": ["rtw","resort"],     "spring2026": ["rtw","resort"], 
+                  "fall2025": ["rtw","resort"],     "spring2025": ["rtw","resort"],
+                  "fall2024": ["rtw","resort"],     "spring2024": ["rtw","resort"] },
+
+      seasons:["fall2026", "spring2026", 
+              "fall2025", "spring2025", 
+              "fall2024", "spring2024"],
+      subcategories: {
+              "fall2026/rtw":   ["women"],      "spring2026/rtw":   ["women"],
+              "fall2026/resort":["women"],      "spring2026/resort":["women"],
+              "fall2025/rtw":   ["women"],      "spring2025/rtw":   ["women"], 
+              "fall2025/resort":["women"],      "spring2025/resort":["women"],
+              "fall2024/rtw":  ["women"],       "spring2024/rtw":   ["women"],
+              "fall2024/resort":["women"],      "spring2024/resort":["women"],
+        }
+      },
+
+      {initial: "CK",
+        name: "Calvin Klein",
+        link: "",
+        categories:{"fall2026": ["rtw"],          "spring2026": ["rtw"], 
+                    "fall2025": ["rtw"],          
+                   },
+
+        seasons:  ["fall2026", "spring2026", 
+                  "fall2025"
+                  ],
+
+        subcategories:{
+                  "fall2026/rtw": ["women"],   "spring2026/rtw": ["women"],
+                  "fall2025/rtw": ["women"],   
+          }
+        },
+
+        { initial: "CG",
+          name:"Cult Gaia",
+          link:"",
+          categories:{"fall2026": ["rtw","resort"],        "spring2026": ["rtw","resort"], 
+                      "fall2025": ["rtw","resort"],        "spring2025": ["rtw","resort"], 
+                      "fall2024": ["rtw","resort"],        "spring2024": ["rtw","resort"] },
+      
+          seasons:   ["fall2026", "spring2026", 
+                      "fall2025", "spring2025", 
+                      "fall2024", "spring2024"],
+          subcategories:{
+                      "fall2026/rtw":   ["women"],      "spring2026/rtw":   ["women"],
+                      "fall2026/resort": ["women"],     "spring2026/resort":["women"],
+                      "fall2025/rtw":   ["women"],      "spring2025/rtw":   ["women"],
+                      "fall2025/resort": ["women"],     "spring2025/resort":["women"],
+                      "fall2024/rtw":   ["women"],      "spring2024/rtw":   ["women"],
+                      "fall2024/resort": ["women"],     "spring2024/resort":["women"],
+            }
+          },
+
+          {initial: "K",
+            name:"Khaite",
+            link:"",
+            categories:{"fall2026": ["rtw","resort"],  "spring2026": ["rtw","resort"],
+                        "fall2025": ["rtw","resort"],  "spring2025": ["rtw","resort"],
+                        "fall2024": ["rtw","resort"],  "spring2024": ["rtw","resort"] },
+          
+            seasons:   ["fall2026",                    "spring2026", 
+                        "fall2025",                    "spring2025", 
+                        "fall2024",                    "spring2024"],
+          
+            subcategories: {
+                        "fall2026/rtw":   ["women"],   "spring2026/rtw":["women"],
+                        "fall2026/resort":["women"],   "spring2026/resort":["women"],
+                        "fall2025/rtw":  ["women"],    "spring2025/rtw":   ["women"],
+                        "fall2025/resort":["women"],   "spring2025/resort":["women"],
+                        "fall2024/rtw":   ["women"],   "spring2024/rtw":   ["women"],
+                        "fall2024/resort":["women"],   "spring2024/resort":["women"],
+              }
+            },
+
 
     {initial: "MM",
       name:"Maria McManus",
@@ -135,6 +251,24 @@ const CITY_DESIGNERS = {
       }
     },
 
+    { initial: "PS",
+      name:"Proenza Scholuer",
+      link:"",
+      categories:{"fall2026": ["rtw"],          "spring2026": ["rtw"], 
+                  "fall2025": ["rtw"],          "spring2025": ["rtw"], 
+                  "fall2024": ["rtw"],          "spring2024": ["rtw"] },
+
+      seasons:   ["fall2026", "spring2026", 
+                  "fall2025", "spring2025",
+                  "fall2024", "spring2024"],
+
+      subcategories:{
+                  "fall2026/rtw": ["women"],   "spring2026/rtw": ["women"],
+                  "fall2025/rtw": ["women"],   "spring2025/rtw": ["women"],
+                  "fall2024/rtw": ["women"],   "spring2024/rtw": ["women"],
+      }
+    },
+
     { initial: "RL",
       name:"Ralph Lauren",
       link:"",
@@ -173,6 +307,61 @@ const CITY_DESIGNERS = {
                 "fall2024/resort":   ["women"],     "spring2024/resort":["women"],
       }
     },
+    { initial: "TB",
+      name:"Thom Browne",
+      link:"",
+      categories:{"fall2026": ["rtw","resort"],                "spring2026": ["rtw","resort"], 
+                  "fall2025": ["rtw","resort"],                "spring2025": ["rtw","resort"], 
+                  "fall2024": ["rtw","resort","couture"],      "spring2024": ["rtw","resort"] },
+  
+      seasons:   ["fall2026", "spring2026", 
+                  "fall2025", "spring2025", 
+                  "fall2024", "spring2024"],
+      subcategories:{
+                  "fall2026/rtw":     ["women","men"],      "spring2026/rtw":   ["women"],
+                  "fall2026/resort":  ["women","men"],     "spring2026/resort":["women"],
+                  "fall2025/rtw":     ["women"],      "spring2025/rtw":   ["women"],
+                  "fall2025/resort":   ["women","men"],     "spring2025/resort":["women","men"],
+                  "fall2024/rtw":     ["women"],      "spring2024/rtw":   ["women"],
+                  "fall2024/resort":   ["women"],     "spring2024/resort":["women"],
+                  "fall2024/couture":   ["women"],
+        }
+      },
+
+      { initial: "TWP",
+        name:"TWP",
+        link:"",
+        categories:{"fall2026": ["rtw","resort"],     "spring2026": ["rtw","resort"], 
+                    "fall2025": ["rtw"] },
+    
+        seasons:   ["fall2026", "spring2026", 
+                    "fall2025"],
+        subcategories: {
+                    "fall2026/rtw":   ["women"],      "spring2026/rtw":   ["women"],
+                    "fall2026/resort":["women"],      "spring2026/resort":["women"],
+                    "fall2025/rtw":   ["women"],
+           }
+        },
+
+      {initial: "SL",
+        name: "Sandy Liang",
+        link: "",
+        categories:{"fall2026": ["rtw","resort"],     "spring2026": ["rtw","resort"], 
+                    "fall2025": ["rtw","resort"],     "spring2025": ["rtw","resort"],
+                    "fall2024": ["rtw","resort"],     "spring2024": ["rtw","resort"] },
+  
+        seasons:["fall2026", "spring2026", 
+                "fall2025", "spring2025", 
+                "fall2024", "spring2024"],
+        subcategories: {
+                "fall2026/rtw":   ["women"],      "spring2026/rtw":   ["women"],
+                "fall2026/resort":["women"],      "spring2026/resort":["women"],
+                "fall2025/rtw":   ["women"],      "spring2025/rtw":   ["women"], 
+                "fall2025/resort":["women"],      "spring2025/resort":["women"],
+                "fall2024/rtw":   ["women"],       "spring2024/rtw":   ["women"],
+                "fall2024/resort":["women"],      "spring2024/resort":["women"],
+          }
+        },
 
   { initial: "UJ",
     name:"Ulla Johnson",
@@ -193,8 +382,17 @@ const CITY_DESIGNERS = {
                 "fall2024/resort":["women"],      "spring2024/resort":["women"],
        }
     },
+
+  
+
+
+
   ],
 },
+
+
+
+
 
 //LONDON COLLECTION
 "London": {
@@ -215,6 +413,25 @@ featured: [
                 "fall2024/rtw": ["women"], "spring2024/rtw": ["women"],
       }
     },
+
+    { initial: "CL",
+      name: "Chopova Lowena",
+      link: "",
+      categories:{"fall2026": ["rtw"], "spring2026": ["rtw"], 
+                  "fall2025": ["rtw"], "spring2025": ["rtw"], 
+                  "fall2024": ["rtw"], "spring2024": ["rtw"] },
+  
+      seasons:   ["fall2026", "spring2026", 
+                  "fall2025", "spring2025", 
+                  "fall2024", "spring2024"],
+  
+      subcategories: {
+                  "fall2026/rtw": ["women"], "spring2026/rtw": ["women"],
+                  "fall2025/rtw": ["women"], "spring2025/rtw": ["women"],
+                  "fall2024/rtw": ["women"], "spring2024/rtw": ["women"],
+      }
+    },
+
   { initial: "EM",
     name: "Erdem",
     link:"",
@@ -253,6 +470,7 @@ featured: [
                 "fall2024/rtw": ["women"],       "spring2024/rtw": ["women"],
     }
   },
+
   { initial: "JW",
     name: "JW Anderson",
     link: "",
@@ -269,6 +487,18 @@ featured: [
                                                 "spring2025/rtw":   ["women","men"],
                 "fall2024/rtw":["women","men"],"spring2024/rtw":   ["men","women"],
                 "fall2024/resort":["women"],   "spring2024/resort":["women"],
+    }
+  },
+
+  { initial: "J",
+    name: "Joseph",
+    link: "",
+    categories: { "fall2026": ["rtw"] },
+
+    seasons: ["fall2026"],
+
+    subcategories: {
+      "fall2026/rtw":["women"],    
     }
   },
 ],
@@ -290,6 +520,7 @@ more: [
                 "fall2024/rtw": ["women"], "spring2024/rtw": ["women"],
     }
   },
+
   { initial: "VB",
     name:"Victoria Beckham",
     link: "",
@@ -310,12 +541,75 @@ more: [
                 "spring2024/rtw": ["women"], "spring2024/resort": ["women"],
     }
   },
+  { initial: "TL",
+    name: "Temperley London",
+    link:"",
+    categories:{"fall2026": ["rtw","resort"], "spring2026": ["rtw","resort"],
+                "fall2025": ["rtw"], "spring2025": ["rtw"],
+                "fall2024": ["rtw"], "spring2024": ["rtw"] },
+
+    seasons:   ["fall2026", "spring2026", 
+                "fall2025", "spring2025", 
+                "fall2024", "spring2024"],
+
+    subcategories:{
+                "fall2026/rtw":  ["women"],   "spring2026/rtw":["women"],
+                "fall2026/resort":["women"],  "spring2026/resort": ["women"],
+                "fall2025/rtw":  ["women"],   "spring2025/rtw":["women"],
+                "fall2024/rtw":  ["women"],   "spring2024/rtw": ["women"],
+    }
+  },
 ],
 },
+
+
+
+
+
+
 
 //MILAN CoLLECTION -------------------
 "Milan": {
   featured: [
+    { initial: "D",
+      name: "Diesel",
+      link:"",
+      categories:{"fall2026": ["rtw","resort"], "spring2026": ["rtw","resort"],
+                  "fall2025": ["rtw","resort"], "spring2025": ["rtw","resort"],
+                  "fall2024": ["rtw","resort"], "spring2024": ["rtw","resort"] },
+  
+      seasons:   ["fall2026", "spring2026", 
+                  "fall2025", "spring2025", 
+                  "fall2024", "spring2024"],
+  
+      subcategories:{
+                  "fall2026/rtw":  ["women"],   "spring2026/rtw":["women"],
+                  "fall2026/resort":["women"],  "spring2026/resort": ["women"],
+                  "fall2025/rtw":  ["women"],   "spring2025/rtw":["women"],
+                  "fall2025/resort": ["women"], "spring2025/resort": ["women"],
+                  "fall2024/rtw":  ["women"],   "spring2024/rtw": ["women"],
+                  "fall2024/resort": ["women"], "spring2024/resort": ["women"],
+      }
+    },
+    { initial: "GA",
+      name: "Giorgio Armani",
+      link: "",
+      categories: { "fall2026": ["rtw"], "spring2026": ["rtw"], 
+                    "fall2025": ["rtw"], "spring2025": ["rtw"], 
+                    "fall2024": ["rtw"], "spring2024": ["rtw"] },
+
+      seasons: ["fall2026", "spring2026", 
+                "fall2025", "spring2025", 
+                "fall2024", "spring2024"],
+
+      subcategories: {
+        "fall2026/rtw":["women","men"],     "spring2026/rtw": ["women","men"],
+        "fall2025/rtw":["women","men"],     "spring2025/rtw": ["women","men"],
+        "fall2024/rtw":["women","men"],     "spring2024/rtw": ["women","men"],
+      }
+    },
+  
+
     { initial: "G",
       name: "Gucci",
       link: "",
@@ -373,6 +667,27 @@ more: [
                                               "spring2024/resort":["women"],
       }
     },
+    { initial: "F",
+      name: "Ferragamo",
+      link: "",
+      categories: { "fall2026": ["rtw","resort"], "spring2026": ["rtw","resort"], 
+                    "fall2025": ["rtw","resort"], "spring2025": ["rtw","resort"], 
+                    "fall2024": ["rtw","resort"], "spring2024": ["rtw","resort"] },
+
+      seasons: ["fall2026", "spring2026", 
+                "fall2025", "spring2025", 
+                "fall2024", "spring2024"],
+
+      subcategories: {
+              "fall2026/rtw":  ["women"],    "spring2026/rtw":   ["women"],    
+              "fall2026/resort":["women"],   "spring2026/resort": ["women"],
+              "fall2025/rtw": ["women"],     "spring2025/rtw":   ["women","men"],
+              "fall2025/resort": ["women"],  "spring2025/resort": ["women"],
+              "fall2024/rtw":["women"],      "spring2024/rtw":["women","men"],
+              "fall2024/resort":["women"],   "spring2024/resort": ["women"],
+      }
+    },
+
     { initial: "BV",
       name: "Bottega Veneta",
       link: "",
@@ -390,6 +705,43 @@ more: [
                                     "spring2025/resort":["women"],
         "fall2024/rtw":   ["women"],"spring2024/rtw":   ["women"], 
         "fall2024/resort":["women"],"spring2024/resort":["women"],
+      }
+    },
+    { initial: "M",
+      name: "Marni",
+      link: "",
+      categories: { "fall2026":["rtw"], 
+                    "fall2025":["rtw"],          "spring2025":["rtw"], 
+                    "fall2024":["rtw","resort"], "spring2024":["rtw","resort"] },
+
+      seasons:["fall2026", 
+               "fall2025", "spring2025", 
+               "fall2024", "spring2024"],
+      subcategories: {
+        "fall2026/rtw":["women"],     
+        "fall2025/rtw":["women"],       "spring2025/rtw":["women"],
+        "fall2024/rtw":["women"],       "spring2024/rtw":["women"],
+        "fall2024/resort":["women"],    "spring2024/resort":["women"],
+      }
+    },
+    { initial: "MM6",
+      name: "MM6 Maison Margiela",
+      link: "",
+      categories: { "fall2026": ["rtw","resort"], "spring2026": ["rtw","resort"], 
+                    "fall2025": ["rtw","resort"], "spring2025": ["rtw","resort"], 
+                    "fall2024": ["rtw","resort"], "spring2024": ["rtw","resort"] },
+
+      seasons: ["fall2026", "spring2026", 
+                "fall2025", "spring2025", 
+                "fall2024", "spring2024"],
+
+      subcategories: {
+              "fall2026/rtw":  ["women"],          "spring2026/rtw":   ["women"],    
+              "fall2026/resort":["women","men"],   "spring2026/resort":["women","men"],
+              "fall2025/rtw": ["women","men"],     "spring2025/rtw":   ["women","men"],
+              "fall2025/resort": ["women","men"],  "spring2025/resort":["women","men"],
+              "fall2024/rtw":["women"],            "spring2024/rtw":["women","men"],
+              "fall2024/resort":["women"],         "spring2024/resort": ["women"],
       }
     },
   ],
@@ -432,9 +784,9 @@ more: [
                 "fall2024", "spring2024"],
 
       subcategories: {
-        "fall2026/rtw":["women","men"], "spring2026/rtw":   ["women","men"],
-        "fall2025/rtw":["women","men"], "spring2025/rtw":   ["women","men"], 
-        "fall2024/rtw":["women","men"], "spring2024/rtw":   ["women","men"],
+        "fall2026/rtw":["women","men"], "spring2026/rtw": ["women","men"],
+        "fall2025/rtw":["women","men"], "spring2025/rtw": ["women","men"], 
+        "fall2024/rtw":["women","men"], "spring2024/rtw": ["women","men"],
       }
     },
     { initial: "MM",
@@ -457,6 +809,7 @@ more: [
         "fall2024/resort":["women"], "spring2024/resort":["women"],
       }
     },
+
     { initial: "M",
       name: "Missoni",
       link: "",
@@ -669,6 +1022,18 @@ const VOGUE_SLUG = {
   "Ralph Lauren":            "ralph-lauren",
   "Tory Burch":              "tory-burch",
   "Ulla Johnson":            "ulla-johnson",
+
+  "Carolina Herrera":        "carolina-herrera",
+  "Calvin Klein":            "calvin-klein-collection",
+  "Cult Gaia":               "cult-gaia",
+  "Proenza Scholuer":        "proenza-schouler",
+  "Altuzarra":               "altuzarra",
+  "Khaite":                  "khaite",
+  "Thom Browne":             "thom-browne",
+  "Anna Sui":                "anna-sui",
+  "Sandy Liang":             "sandy-liang",
+  "TWP":                     "twp",
+
   //London
   "Burberry":                "burberry-prorsum",
   "Erdem":                   "erdem",
@@ -676,15 +1041,27 @@ const VOGUE_SLUG = {
   "JW Anderson":             "j-w-anderson",
   "Roksanda":                "roksanda",
   "Victoria Beckham":        "victoria-beckham",
+  "Joseph":                  "joseph",
+  "Temperley London":        "temperley-london",
+  "Chopova Lowena":          "chopova-lowena",
+
+
   //Milan
+  "Diesel":                   "diesel",
   "Gucci":                   "gucci",
+  "Giorgio Armani":          "giorgio-armani",
   "Prada":                   "prada",
   "Fendi":                   "fendi",
+  "Ferragamo":               "salvatore-ferragamo",
   "Bottega Veneta":          "bottega-veneta",
   "Valentino":               "valentino",
   "Dolce&Gabbana":           "dolce-gabbana",
   "Max Mara":                "max-mara",
   "Missoni":                 "missoni",
+  "Marni":                   "marni",
+  "MM6 Maison Margiela":     "mm6-maison-martin-margiela",
+
+
   // Paris
   "Chanel":                  "chanel",
   "Louis Vuitton":           "louis-vuitton",
